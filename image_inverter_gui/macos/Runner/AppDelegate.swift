580e21355fff5b2f4d37a9f7ead0fd5102a4de65
nmccloud <nathan.mccloud898@gmail.com>
2024-08-05 17:12:52 -0500

    window naming

--- image_inverter_gui/macos/Runner/AppDelegate.swift
+++ image_inverter_gui/macos/Runner/AppDelegate.swift
@@ -6,4 +6,9 @@ class AppDelegate: FlutterAppDelegate {
   override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
     return true
   }
+   override func applicationDidFinishLaunching(_ notification: Notification) {
+    if let mainWindow = NSApplication.shared.windows.first {
+      mainWindow.title = "Image Inverter"
+    }
+    super.applicationDidFinishLaunching(notification)
 }
